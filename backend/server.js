80862380d57bebe0366260cfa427d3e5e38667ef
Muhammad Iqbal <muhadiqbal2@gmail.com>
2021-04-21 13:28:28 +0700

    tes

--- backend/server.js
+++ backend/server.js
@@ -28,14 +28,10 @@ app.get('/api/config/paypal', (req, res) => {
 });
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.get('*', function (req, res) {
-  const index = path.join(__dirname, 'build', 'index.html');
-  res.sendFile(index);
-});
-  // app.use(express.static(path.join(__dirname, '/frontend/build')));
-  // app.get('*', (req, res) =>
-  //   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-  // );
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+  );
   // app.get('/', (req, res) => {
   //   res.send('Server is ready');
   // });
