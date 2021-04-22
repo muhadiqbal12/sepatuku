522f0557b5def82ba69162e10e75be9731fbd350
Muhammad Iqbal <muhadiqbal2@gmail.com>
2021-04-22 20:13:38 +0700

    tes

--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,45 @@
 import express from 'express';
+import cors from 'cors';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 import path from 'path';
-import productRouter from './routers/productRouter.js';
-import userRouter from './routers/userRouter.js';
-import orderRouter from './routers/orderRouter.js';
-import uploadRouter from './routers/uploadRouter.js';
+import config from './config';
+import userRouter from './routers/userRouter';
+import orderRouter from './routers/orderRouter';
+import productRouter from './routers/productRouter';
+import uploadRouter from './routers/uploadRouter';
 
-
-dotenv.config();
-
+mongoose
+  .connect(config.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('Connected to mongodb.');
+  })
+  .catch((error) => {
+    console.log(error.reason);
+  });
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/vendingmachine', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+app.use(cors());
+app.use(bodyParser.json());
 app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
-app.get('/api/config/paypal', (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+app.get('/api/paypal/clientId', (req, res) => {
+  res.send({ clientId: config.PAYPAL_CLIENT_ID });
 });
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
-  app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-  );
-  // app.get('/', (req, res) => {
-  //   res.send('Server is ready');
-  // });
-
+app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
+app.use(express.static(path.join(__dirname, '/../frontend')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/../frontend/index.html'));
+});
 app.use((err, req, res, next) => {
-  res.status(500).send({ message: err.message });
+  const status = err.name && err.name === 'ValidationError' ? 400 : 500;
+  res.status(status).send({ message: err.message });
 });
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Berjalan di http://localhost:${port}`);
+app.listen(config.PORT, () => {
+  console.log('serve at http://localhost:5000');
 });
